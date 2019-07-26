18eb45fa005f9909ec2fb809a7fe7a829f0e39af
Vincent White <xstrengthofonex@gmail.com>
2019-07-26 23:49:30 +0900

    Added axios mock

--- src/__tests__/services/UserService.test.js
+++ src/__tests__/services/UserService.test.js
@@ -1,13 +1,21 @@
 
 import userService from "../../services/UserService";
+import axios from "axios";
+
+jest.mock("axios");
 
 
 describe("UserService", () => {
     it("registers", async () => {
+        const token = "token";
+        const resp = {data: {userId: "UserId", token: token}};
         const userData = {email: "email@example.com",
             username: "Username", password: "12345678"};
+        axios.post.mockResolvedValue(resp);
+
         const result = await userService.register(userData);
+
         expect(userService.userId).toEqual(result);
-        expect(localStorage.getItem("token")).not.toBeNull();
+        expect(localStorage.getItem("token")).toEqual(token);
     });
 });
